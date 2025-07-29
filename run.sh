bb71005dd5dd1867edca490cf68a2d1bc8201e30
Suresh Kannan <sureshk@athenahealth.com>
2025-07-29 12:33:09 +0530

    cosmetic

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-npx tsx src/client.ts \
+npx tsx src/${WHAT:-client}.ts \
   $URL 8001 \
   --static-oauth-client-info "{\"client_id\": \"$OKTA_CLIENT_ID\", \"client_secret\": \"$OKTA_CLIENT_SECRET\"}" \
   --static-oauth-client-metadata "{\"scope\": \"openid profile offline_access $CUSTOM_SCOPE\"}" \
